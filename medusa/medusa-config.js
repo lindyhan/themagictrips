8ef1cb1f51899ed04e512efa979fd9f832c7fe45
lindyhan <lindyhan@gmail.com>
2024-11-16 12:51:50 +0800

    -

--- medusa/medusa-config.js
+++ medusa/medusa-config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
+      port: process.env.PORT || 9000,
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
       authCors: process.env.AUTH_CORS,
